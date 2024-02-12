f54974086656c965fb85f5c5fe27cfcf3ba56ec1
Protolaw <alexforbesdilaw@gmail.com>
2024-02-12 15:03:14 +0300

    modified files to merge

--- raman_prog.f90
+++ raman_prog.f90
@@ -1,30 +1,36 @@
 PROGRAM le
 IMPLICIT NONE
-INTEGER::k,i,tot_struct = 14487,j1 = 2
+INTEGER::k,i,tot_struct = 19999,j1 = 2
 REAL::eps_data(1000000,10),ac(1000000,10)
 REAL::s = 0,sum_avg,sum_up,sum_down,x,sum_fourier_cos,sum_fourier_sin,dt,integration
 REAL::h_k,r,w,k_in,w_in,temp,exp_part,wavenum,f,pi,n_in
 
 pi = (22.0/7.0)
 
-OPEN(UNIT=9,FILE='auto_correlation_func_11_model')
+OPEN(UNIT=9,FILE='./tables/CO2_acorr_sep.csv')
 OPEN(UNIT=16,FILE='Raman_11_model')
 
-PRINT*,"Give wavelength (in nm) of incident light:  "
-READ*,n_in 
+! PRINT*,"Give wavelength (in nm) of incident light:  "
+! READ*,n_in 
+
+n_in = 532
 
 w_in = (45.613245464/n_in)                  !!! in a.u 
 
 PRINT*,w_in
 
-PRINT*,"Give the temp(in K): "
-READ*,temp
+! PRINT*,"Give the temp(in K): "
+! READ*,temp
+
+temp = 300
 
 DO k = 1,tot_struct
   READ(9,*)(ac(k,i),i=1,j1)
 ENDDO
 
 dt = ac(2,1) - ac(1,1)
+
+PRINT*, dt
 
 h_k = 3.15775*100000     !!!!!! h_bar/K value (in Sec.K )  (h = planck const, K = boltzmann)
 
